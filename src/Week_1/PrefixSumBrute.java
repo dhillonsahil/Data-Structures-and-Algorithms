3e4776e50071d59b89d769d0f9b23e9d9b6ab1c1
Sahil <sahil2742002@gmail.com>
2023-04-19 16:50:02 +0530

    Prefix Sum Brute Force Method

---
+++ src/Week_1/PrefixSumBrute.java
@@ -0,0 +1,24 @@
+public class PrefixSumBrute {
+    public static void main(String[] args){
+        int arr[][]={
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1}
+        };
+
+        int r1=0;int c1=2; int r2=3;int c2=3;
+
+        
+        int sum=0;
+        for(int i=r1;i<=r2;i++){
+            for(int j=c1;j<=c2;j++){
+                sum+=arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
+// time complexity = O(m*n) where m = r2-r1 and n = c2-c1;
+// space complexity = O(1)
