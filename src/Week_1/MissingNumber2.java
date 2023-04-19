a9b305c92da23c7da85c9c10b90d89b698002ccf
Sahil <sahil2742002@gmail.com>
2023-04-19 08:31:21 +0530

    solved overflow problem
    solved overflow problem if we find sum of n natural number if n is larger it may possibly overflow in MissingNumber.java so that issue is solved in this file

---
+++ src/Week_1/MissingNumber2.java
@@ -0,0 +1,15 @@
+package Week_1;
+
+public class MissingNumber2 {
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,5};
+        System.out.println("MissingNumber number is "+ missing(arr, arr.length));
+    }
+    public static int missing(int[] arr , int n){
+        int total =1;
+        for(int i=2;i<=n+1;i++){
+            total+=i;
+            total-=total=arr[i-2];
+        }return total;
+    }
+}
