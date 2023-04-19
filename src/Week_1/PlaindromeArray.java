a5486bb85d889c0e385b480f16262bf47f052bf1
Sahil <sahil2742002@gmail.com>
2023-04-19 07:34:11 +0530

    Plaindrome array

---
+++ src/Week_1/PlaindromeArray.java
@@ -0,0 +1,27 @@
+package Week_1;
+
+
+public class PlaindromeArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,2,1};
+        int n = arr.length-1;
+        boolean flag=true;
+        for(int i=0;i<arr.length/2;i++){
+            if(arr[i]==arr[n-i]){
+                continue;
+            }else{
+            flag=false;
+            break;
+        }
+    }
+    if(flag==true){
+        System.out.println("Plaindrome ");
+    }else{
+        System.out.println("Not Palindrome");
+    }
+}
+
+}
+
+// time complexity O(n)
+// space complexity O(1)
