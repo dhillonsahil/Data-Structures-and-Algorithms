60f69b05ecd17d37c875d9a1add7ab719892bc7a
Sahil <sahil2742002@gmail.com>
2023-04-25 19:24:44 +0530

    sort descending using BubbleSort

---
+++ src/Week_2/Assignment/BubbleSort_Desc.java
@@ -0,0 +1,21 @@
+package Week_2.Assignment;
+
+import java.util.*;
+public class BubbleSort_Desc {
+    public static void main(String[] args) {
+        int[] arr={3,5,1,6,0};
+        bubbleSort(arr);
+        System.out.println("Descending order Of Array is : "+Arrays.toString(arr));
+    }
+    public static void bubbleSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
