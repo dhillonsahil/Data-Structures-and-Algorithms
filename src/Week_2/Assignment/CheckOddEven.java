a8488c6cf6f35beab4628d16a571f010c942392e
Sahil <sahil2742002@gmail.com>
2023-04-27 16:29:02 +0530

    Check Odd Even number

---
+++ src/Week_2/Assignment/CheckOddEven.java
@@ -0,0 +1,26 @@
+package Week_2.Assignment;
+
+import java.util.*;
+
+public class CheckOddEven {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int x = scan.nextInt();
+        int binaryNum = 0;
+        int pow = 1;
+        while (x > 0) {
+            int remainder = x % 2;
+            binaryNum += remainder * pow;
+            pow *= 10;
+            x /= 2;
+        }
+        if((binaryNum & 1) == 0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+        scan.close();
+    }
+}
